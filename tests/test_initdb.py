578eedfaa12a5794dd2e55784bac136a43a55fe1
James Tombleson <luther38@gmail.com>
2020-08-28 05:14:31 +0000

    adding unit tests to sql functions

--- tests/test_initdb.py
+++ tests/test_initdb.py
@@ -1,7 +1,7 @@
 
 from newsbot.initdb import InitDb
 from newsbot.db import DB, Base
-from newsbot.tables import Articles, Sources
+from newsbot.tables import Articles, Sources, DiscordWebHooks
 from pathlib import Path
 from os import remove
 
@@ -22,4 +22,43 @@ class Test_DB():
         assert len(res) == 1
 
     #def test_runDatabaseTasks(self)
-        
+
+class Test_Sources():
+    def test_add(self):
+        s = Sources(name="Source01", url="void")
+        s.add()
+
+        res = s.findAllByName()
+        if res[0].name == 'Source01':
+            assert True
+
+    def test_clearTable(self):
+        Sources().clearTable()
+        s = Sources(name='Sources01')
+        res = s.findAllByName()
+        if len(res) == 0:
+            assert True
+
+class Test_DiscordWebHooks():
+    def test_init00(self):
+        d = DiscordWebHooks(name='test', key='url')
+        if d.name == "test" and d.key == "url":
+            assert True
+
+    def test_init01(self):
+        d = DiscordWebHooks(name='test')
+        if d.name == 'test' and d.key == '':
+            assert True
+
+    def test_init02(self):
+        d = DiscordWebHooks(key='url')
+        if d.name == '' and d.key == 'url':
+            assert True
+    def test_init03(self):
+        d = DiscordWebHooks()
+        if d.name == '' and d.key == '':
+            assert True
+
+    def test_add(self):
+        d = DiscordWebHooks(name="test", key='badurl').add()
+        d
