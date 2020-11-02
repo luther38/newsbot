01d470252d8311f4ef4b75db3004d6c83e64149a
James Tombleson <luther38@gmail.com>
2020-11-02 20:05:37 +0000

    moved the current calls to the Logger class to avoid breaking things.  This is now ready to take the SQL calls.

--- newsbot/logger.py
+++ newsbot/logger.py
@@ -9,3 +9,21 @@ class Logger:
     def init(self) -> logger:
         logger.add("./mounts/logs/newsbot.log", rotation="500 MB")
         return logger
+
+    def debug(self, message: str) -> None:
+        self.logger.debug(message)
+
+    def info(self, message: str) -> None:
+        self.logger.info(message)
+
+    def warning(self, message: str) -> None:
+        self.logger.warning(message)
+        
+    def error(self, message: str) -> None:
+        self.logger.error(message)
+
+    def critical(self, message: str) -> None:
+        self.logger.critical(message)
+
+    def __writeDb__(self,message, type) -> None:
+        pass
