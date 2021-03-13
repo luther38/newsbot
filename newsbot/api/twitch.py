1a4e86c462e10e27e49c3508f986aa507f262563
James Tombleson <luther38@gmail.com>
2021-03-13 14:41:47 +0000

    ran black

--- newsbot/api/twitch.py
+++ newsbot/api/twitch.py
@@ -1,22 +1,29 @@
-
-from newsbot import logger
+from newsbot.logger import Logger
 from os import getenv
 from requests import post, get
 from json import loads, JSONEncoder
 from typing import Dict, List, NamedTuple
 
-class TwitchAuth():
-    def __init__(self, access_token: str = '', expires_in: int = 0, token_type: str = '', client_id: str = ''):
+
+class TwitchAuth:
+    def __init__(
+        self,
+        access_token: str = "",
+        expires_in: int = 0,
+        token_type: str = "",
+        client_id: str = "",
+    ):
         self.access_token = access_token
         self.expires_in = expires_in
         self.token_type = token_type
         self.client_id = client_id
         pass
 
-class TwitchChannel():
+
+class TwitchChannel:
     ## might go away
-    def __init__(self, obj: Dict= {}):
-        self.broadcaster_language: str = obj.get('broadcaster_language')
+    def __init__(self, obj: Dict = {}):
+        self.broadcaster_language: str = obj.get("broadcaster_language")
         self.display_name: str = obj.get("display_name")
         self.game_id: int = int(obj.get("game_id"))
         self.id: int = int(obj.get("id"))
@@ -27,7 +34,8 @@ class TwitchChannel():
         self.started_at: str = str(obj.get("started_at"))
         self.game_data = TwitchGameData()
 
-class TwitchUser():
+
+class TwitchUser:
     def __init__(self, obj: Dict):
         self.id = obj.get("id")
         self.login = obj.get("login")
@@ -39,7 +47,8 @@ class TwitchUser():
         self.offline_image_url = obj.get("offline_image_url")
         self.view_count = obj.get("view_count")
 
-class TwitchVideo():
+
+class TwitchVideo:
     def __init__(self, obj: Dict):
         self.id = obj.get("id")
         self.user_id = obj.get("user_id")
@@ -57,7 +66,8 @@ class TwitchVideo():
         self.duration = obj.get("duration")
         pass
 
-class TwitchClip():
+
+class TwitchClip:
     def __init__(self, obj: Dict = {}):
         self.id = obj.get("id")
         self.url = obj.get("url")
@@ -75,8 +85,9 @@ class TwitchClip():
         self.thumbnail_url = obj.get("thumbnail_url")
         pass
 
-class TwitchGameData():
-    def __init__(self, obj: Dict= {}):
+
+class TwitchGameData:
+    def __init__(self, obj: Dict = {}):
         self.id = 0
         try:
             self.id = int(obj.get("id"))
@@ -87,21 +98,23 @@ class TwitchGameData():
         self.box_art_url: str = obj.get("box_art_url")
         pass
 
-class TwitchStream():
-    def __init__(self, obj: Dict = {}):
-        self.id = obj.get('id')
-        self.user_id = obj.get('user_id')
-        self.user_name = obj.get('user_name')
-        self.game_id = obj.get('game_id')
-        self.type = obj.get('type')
-        self.title = obj.get('title')
-        self.viewer_count = obj.get('viewer_count')
-        self.started_at = obj.get('started_at')
-        self.language = obj.get('language')
-        self.thumbnail_url = obj.get("thumbnail_url")
-        self.tags_ids = obj.get('tag_ids')
 
-class TwitchAPI():
+class TwitchStream:
+    def __init__(self, obj: Dict = {}):
+        self.id = obj.get("id")
+        self.user_id = obj.get("user_id")
+        self.user_name = obj.get("user_name")
+        self.game_id = obj.get("game_id")
+        self.type = obj.get("type")
+        self.title = obj.get("title")
+        self.viewer_count = obj.get("viewer_count")
+        self.started_at = obj.get("started_at")
+        self.language = obj.get("language")
+        self.thumbnail_url = obj.get("thumbnail_url")
+        self.tags_ids = obj.get("tag_ids")
+
+
+class TwitchAPI:
     def __init__(self):
         self.uri: str = "https://id.twitch.tv"
         self.apiUri: str = "https://api.twitch.tv/helix"
@@ -113,53 +126,61 @@ class TwitchAPI():
         """
         client_id = str(getenv("NEWSBOT_TWITCH_CLIENT_ID"))
         client_secret = str(getenv("NEWSBOT_TWITCH_CLIENT_SECRET"))
-        scopes = 'user:read:email'
+        scopes = "user:read:email"
         uri = f"https://id.twitch.tv/oauth2/token?client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials&scopes={scopes}"
-    
+
         res = post(uri)
         if res.status_code != 200:
-            logger.error(res.text)
+            Logger().error(res.text)
             return TwitchAuth()
         else:
             token = loads(res.content)
-            o = TwitchAuth(access_token=token['access_token'], 
-                expires_in=token['expires_in'], 
-                token_type=token['token_type'],
-                client_id=client_id)
+            o = TwitchAuth(
+                access_token=token["access_token"],
+                expires_in=token["expires_in"],
+                token_type=token["token_type"],
+                client_id=client_id,
+            )
             return o
 
     def getUser(self, auth: TwitchAuth, username: str) -> None:
         uri = f"{self.apiUri}/users?login={username}"
         res = get(uri, headers=self.__header__(auth))
         if res.status_code != 200:
-            logger.error(f"Failed to get user information. StatusCode: {res.status_code}, Error: {res.text}")
+            Logger().error(
+                f"Failed to get user information. StatusCode: {res.status_code}, Error: {res.text}"
+            )
         else:
             json = loads(res.content)
-            if len(json['data']) == 1:
-                user = TwitchUser(json['data'][0])
-            else: 
-                logger.error(f"Did not get a usable object.")
+            if len(json["data"]) == 1:
+                user = TwitchUser(json["data"][0])
+            else:
+                Logger().error(f"Did not get a usable object.")
                 user = TwitchUser({})
             return user
 
-    def searchForUser(self, auth: TwitchAuth, username: str = '') -> None:
-        if username == '':
-            logger.error(f"Request to search for user was requested but no user was given.")
+    def searchForUser(self, auth: TwitchAuth, username: str = "") -> None:
+        if username == "":
+            Logger().error(
+                f"Request to search for user was requested but no user was given."
+            )
         else:
             uri: str = f"{self.apiUri}/search/channels?query={username}"
             header = self.__header__(auth)
             res = get(uri, headers=header)
             if res.status_code != 200:
-                logger.error(f"Attempted to pull user information but failed. status_code: {res.status_code}, output: {res.text}")
+                Logger().error(
+                    f"Attempted to pull user information but failed. status_code: {res.status_code}, output: {res.text}"
+                )
             else:
                 l = list()
                 j = loads(res.content)
-                for i in j['data']:
+                for i in j["data"]:
                     # Convert the Json date to an object
                     stream = TwitchChannel(i)
                     # Get the game details
-                    #stream.game_data = self.getGame(auth,stream.game_id)
-                    #video = self.getVideos(auth=auth, user_id=stream.id)
+                    # stream.game_data = self.getGame(auth,stream.game_id)
+                    # video = self.getVideos(auth=auth, user_id=stream.id)
                     l.append(stream)
                 return l
 
@@ -169,17 +190,21 @@ class TwitchAPI():
         res = get(uri, headers=headers)
 
         if res.status_code != 200:
-            logger.error(f"Attempted to get Twich Game data but failed on game_id: {game_id}. output: {res.text}")
+            Logger().error(
+                f"Attempted to get Twich Game data but failed on game_id: {game_id}. output: {res.text}"
+            )
             return TwitchGameData()
         else:
             j = loads(res.content)
-            if len(j['data']) != 0:
-                game = TwitchGameData(j['data'][0])
-            else: 
+            if len(j["data"]) != 0:
+                game = TwitchGameData(j["data"][0])
+            else:
                 game = TwitchGameData()
             return game
 
-    def getVideos(self, auth: TwitchAuth, id: int = 0, user_id: int = 0, game_id: int = 0) -> List[TwitchVideo]:
+    def getVideos(
+        self, auth: TwitchAuth, id: int = 0, user_id: int = 0, game_id: int = 0
+    ) -> List[TwitchVideo]:
         uri = f"{self.apiUri}/videos"
         if id != 0:
             uri = f"{uri}?id={id}"
@@ -187,70 +212,79 @@ class TwitchAPI():
             uri = f"{uri}?user_id={user_id}"
         elif game_id != 0:
             uri = f"{uri}?game_id={game_id}"
-        
+
         res = get(uri, headers=self.__header__(auth))
         videos = list()
         if res.status_code != 200:
-            logger.error(f"Failed to request videos")
+            Logger().error(f"Failed to request videos")
             return videos
         else:
             json = loads(res.content)
-            for i in json['data']:
+            for i in json["data"]:
                 videos.append(TwitchVideo(i))
             return videos
 
-    def getClips(self, auth: TwitchAuth, user_id: int = 0, clip_id: str = '', game_id: int= 0) -> List[TwitchClip]:
+    def getClips(
+        self, auth: TwitchAuth, user_id: int = 0, clip_id: str = "", game_id: int = 0
+    ) -> List[TwitchClip]:
         uri = f"{self.apiUri}/clips"
         if user_id != 0:
             uri = f"{uri}?broadcaster_id={user_id}"
-        elif clip_id != '':
+        elif clip_id != "":
             uri = f"{uri}?id={clip_id}"
         elif game_id != 0:
             uri = f"{uri}?game_id={game_id}"
         else:
-            logger.error(f"Clips was requested but was given invalid parameters, returning empty object.")
+            Logger().error(
+                f"Clips was requested but was given invalid parameters, returning empty object."
+            )
             return ""
 
         res = get(uri, headers=self.__header__(auth))
         clips = list()
         if res.status_code != 200:
-            logger.error(f"Clips request returnd a bad status_code. Code: {res.status_code}, Error: {res.text}")
+            Logger().error(
+                f"Clips request returnd a bad status_code. Code: {res.status_code}, Error: {res.text}"
+            )
             return clips
         else:
             json = loads(res.content)
-            for i in json['data']:
+            for i in json["data"]:
                 clips.append(TwitchClip(i))
-        
+
         return clips
 
-    def getStreams(self, auth: TwitchAuth, game_id: int = 0, user_id: int = 0, user_login: str = '') -> None:
+    def getStreams(
+        self, auth: TwitchAuth, game_id: int = 0, user_id: int = 0, user_login: str = ""
+    ) -> None:
         uri = f"{self.apiUri}/streams"
         if game_id != 0:
             uri = f"{uri}?game_id={game_id}"
         elif user_id != 0:
             uri = f"{uri}?user_id={user_id}"
-        elif user_login != '':
+        elif user_login != "":
             uri = f"{uri}?user_login={user_login}"
         else:
             pass
-       
+
         res = get(uri, headers=self.__header__(auth))
         streams = list()
         if res.status_code != 200:
-            logger.error(f"Streams request returned a bad status_code. Code: {res.status_code}, Error: {res.test}")
+            Logger().error(
+                f"Streams request returned a bad status_code. Code: {res.status_code}, Error: {res.test}"
+            )
             return streams
         else:
             json = loads(res.content)
-            if len(json['data']) == 0:
+            if len(json["data"]) == 0:
                 streams.append(TwitchStream())
-            for i in json['data']:
+            for i in json["data"]:
                 streams.append(TwitchStream(i))
-        
+
         return streams
 
     def __header__(self, auth: TwitchAuth) -> Dict:
         return {
-            'Authorization': f"Bearer {auth.access_token}",
-            'Client-ID': f"{auth.client_id}"
+            "Authorization": f"Bearer {auth.access_token}",
+            "Client-ID": f"{auth.client_id}",
         }
-
