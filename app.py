b3f2d38e91449a0641f9102b26a88d1c50013874
James Tombleson <luther38@gmail.com>
2021-03-21 10:35:13 -0700

    cleaned up the startup file

--- app.py
+++ app.py
@@ -1,14 +1,2 @@
 from newsbot.startup import Startup
-
-# from sel import SeleniumTest
-# env.newDatabase: bool = True
-# env.readEnv()
-# dbPath = Path("./mounts/database/newsbot.db")
-# if dbPath.exists:
-#    env.newDatabase = False
-
-# system("alembic upgrade head")
-# SeleniumTest().getArticles()
-
-s = Startup()
-s.startProgram()
+Startup()
