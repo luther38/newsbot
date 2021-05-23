bfdd689c151c6d637d7ef37f8302ae3972dd8d9f
James Tombleson <James.Tombleson@suddath.com>
2021-05-23 23:21:05 +0000

    Forgot to add the name column.. I am not sure how I did that

--- newsbot/core/sql/tables/discordWebHooks.py
+++ newsbot/core/sql/tables/discordWebHooks.py
@@ -15,8 +15,6 @@ from newsbot.core.sql import database, Base
 from newsbot.core.sql.tables import Articles, ITables
 from newsbot.core.sql.exceptions import FailedToAddToDatabase
 
-
-
 class DiscordWebHooks(Base, ITables):
     __tablename__ = "discordwebhooks"
     id = Column(String, primary_key=True)
@@ -37,6 +35,7 @@ class DiscordWebHooks(Base, ITables):
         url: str = "",
         fromEnv: bool = False
     ) -> None:
+        self.name = name
         self.id = str(uuid.uuid4())
         self.server = server
         self.channel = channel
